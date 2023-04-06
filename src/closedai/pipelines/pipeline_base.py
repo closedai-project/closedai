f9516b679b72b187c17fa2d6dfe3a5fc2586ed84
nateraw <nxr9266@g.rit.edu>
2023-04-05 22:29:00 -0400

    :art: restructure

--- src/closedai/pipelines/pipeline_base.py
+++ src/closedai/pipelines/pipeline_base.py
@@ -1,15 +1,18 @@
+from typing import List
+
+
 class ClosedAIPipeline:
     def __init__(self, *args, **kwargs):
         pass
 
-    def generate_completion(self, text, **kwargs):
+    def generate_completion(self, text: str, **kwargs):
         raise NotImplementedError("Completions not implemented for this pipeline")
 
-    def get_completion(self, text, **kwargs):
+    def get_completion(self, text: str, **kwargs):
         return "".join(list(self.generate_completion(text, **kwargs)))
 
-    def generate_chat_completion(self, text, **kwargs):
+    def generate_chat_completion(self, messages: List[str], **kwargs):
         raise NotImplementedError("Chat completions not implemented for this pipeline")
 
-    def get_chat_completion(self, text, **kwargs):
-        return "".join(list(self.generate_completion(text, **kwargs)))
+    def get_chat_completion(self, messages: List[str], **kwargs):
+        return "".join(list(self.generate_chat_completion(messages, **kwargs)))
